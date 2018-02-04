af5198f8bb56a4845f4e081fd8a553f935c94cde
Jose Juan Montes <jjmontes@gmail.com>
2018-02-04 18:28:17 +0100

    Updated version to 1.0.3.

--- clementineremote/__init__.py
+++ clementineremote/__init__.py
@@ -18,4 +18,4 @@ from .clementine import ClementineRemote
 
 __author__ = 'Jose Juan Montes [@jjmontesl]'
 __description__ = 'Python library for the Clementine Music Player remote protocol.'
-__version__ = '1.0.2'
+__version__ = '1.0.3'
